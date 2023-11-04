6391868d553fca23515cf3cd980865c3ae831592
Carlos Flores <carlos@floresbenavides.com>
2023-11-03 20:54:38 -0600

    migrate server to expressjs

--- app/webserver.js
+++ app/webserver.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const express = require('express')
 const { readFileSync } = require('fs')
 const { proxy } = require('./proxy')
 
@@ -14,36 +14,38 @@ readFileSync('./.env', 'utf8')
 const { PORT = 8080 } = process.env
 
 // servidor web para procesar las llamadas
-const server = http.createServer((req, res) => {
-  // validamos que el usuario esté buscando una ciudad definida
-  // y que solo sean los métodos aceptados
-  const urlMatch = req.url.replace('/', '').toUpperCase()
+const app = express()
+
+const gateway = (req, res, next) => {
+  const urlMatch = req.params.city.toUpperCase()
   const targetUrl = process.env[urlMatch]
 
-  if (!targetUrl || !['GET', 'OPTIONS'].includes(req.method)) {
-    res.writeHead(404, { 'Content-Type': 'text/plain' })
-    res.end('Esta no es la página que estás buscando')
+  // validamos que el usuario esté buscando una ciudad definida
+  // y que solo sean los métodos aceptados
+  if (!targetUrl) {
+    res.status(404).send('Esta no es la página que estás buscando')
     return
   }
   const allowedOrigins = process.env.ORIGINS.split(',')
-  const { headers } = req
-  const { origin } = headers
-
+  const origin = req.get('origin')
   // CORS para unicamente orígenes válidos
   if (!allowedOrigins.includes(origin)) {
-    res.writeHead(403, { 'Content-Type': 'text/plain' })
-    res.end('Acceso no autorizado')
+    res.status(403).send('Acceso no autorizado')
     return
   }
-
   // Si ya llegó hasta acá no hay problema con CORS ni respuestas
-  res.setHeader('Access-Control-Allow-Origin', origin)
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  res.setHeader('Content-Type', 'application/json')
+  res.set('Access-Control-Allow-Origin', origin)
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.set('Access-Control-Allow-Headers', 'Content-Type')
+  res.set('Content-Type', 'application/json')
+  req.targetUrl = targetUrl
+  next()
+}
+app.options('/:city', gateway, (_req, res) => { res.sendStatus(200) })
+app.get('/:city', gateway, (req, res) => {
+  console.log(`-> Querying calendar for ${req.params.city}`)
 
-  console.log(`-> Querying calendar for ${req.url.replace('/', '')}`)
-  const proxyReq = proxy(targetUrl, res)
+  const proxyReq = proxy(req.targetUrl, res)
 
   // Route handlers para todo lo demás
   proxyReq.on('error', (err) => {
@@ -51,15 +53,9 @@ const server = http.createServer((req, res) => {
     res.writeHead(500)
     res.end('Error en la solicitud proxy')
   })
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200)
-    res.end()
-  } else if (req.method === 'GET') {
-    proxyReq.end()
-  }
+  proxyReq.end()
 })
 
-// Iniciamos el server
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Proxy escuchando en el puerto ${PORT}`)
 })
